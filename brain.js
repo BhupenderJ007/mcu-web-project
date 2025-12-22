ed14b7a03173f4a9283bcce87ab6fb1c8820e1c0
Bhupender Jangra <bhupenderj@Bhupenders-MacBook-Air.local>
2025-12-22 19:53:42 +0530

    added animation

--- brain.js
+++ brain.js
@@ -1,9 +1,11 @@
 let upper = document.querySelector('.upper');
 let boxes = document.querySelectorAll('.box');
 let imgfm = document.getElementById('imgfm');
+let imgh = document.querySelector('.imgh');
 
 boxes.forEach(box => { 
     box.addEventListener('click', () => {
+        imgh.classList.add('aniF');
         upper.style.display = 'flex';
         imgfm.src = box.querySelector('img').src;
         imgfm.alt = box.querySelector('p').textContent;
@@ -11,6 +13,7 @@ boxes.forEach(box => {
 });
 
 upper.addEventListener('click', () => {
+    imgh.classList.remove('aniF');
     upper.style.display = 'none';
     imgfm.src = '';
     imgfm.alt = '';
